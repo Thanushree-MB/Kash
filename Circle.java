77ff7cead8d2823e948336de1057a5f61438a435
Thanushree-MB <thanushreemb12@gmail.com>
2025-03-03 15:42:00 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,13 +1,12 @@
-public class Circle 
-{
-    public static void main(String[] args) 
+public class Circle {
+    public static void main(String[] args)
     {
-        int r;
-        double pi = 3.14, area;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter radius of circle: ");
-        r = s.nextInt();
-        area = pi * r * r;
-        System.out.println("Area of circle: "+area);
-    }            
+        int radius;
+        double pi = 3.142, area;
+        radius = 5;
+        // calculating the area of the circle
+        area = pi * radius * radius;
+        // printing the area of the circle
+        System.out.println("Area of circle is :" + area);
+    }
 }
